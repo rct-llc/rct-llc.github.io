cf8c2e8afe0714f8d1f436bd00a6233030adeaeb
kurt <kurt@rockcreektechnologiesllc.com>
2023-05-05 00:35:28 -0600

    working button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,16 @@
 // alert('woah');
-console.log('hi');
+// console.log('hi');
+window.addEventListener('DOMContentLoaded', function(event) {
+  var menuItem = document.querySelectorAll('.dropdown-button');
+  if (menuItem) {
+    for (var i = 0; i < menuItem.length; i++) {
+      var button = menuItem[i];
+      button.addEventListener('click', function(event) {
+        event.target.classList.toggle('dropdown-active');
+      });
+      button.addEventListener('blur', function(event) {
+        event.target.classList.remove('dropdown-active');
+      });
+    }
+  }
+});
