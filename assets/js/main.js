b446c006315c590fce2bb4c67cb9e167a05f78be
kurt <kurt@rockcreektechnologiesllc.com>
2023-05-05 23:24:54 -0600

    set location globally

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
 // alert('woah');
 // console.log('hi');
-window.addEventListener('DOMContentLoaded', function(event) {
-  var menuItem = document.querySelectorAll('.dropdown-button');
-  if (menuItem) {
-    for (var i = 0; i < menuItem.length; i++) {
-      var button = menuItem[i];
-      button.addEventListener('click', function(event) {
-        event.target.classList.toggle('dropdown-active');
-      });
-      button.addEventListener('blur', function(event) {
-        event.target.classList.remove('dropdown-active');
-      });
-    }
+window.addEventListener('DOMContentLoaded', function(domEvent) {
+  var siteSelectorButton = document.getElementById('site-selector-button');
+  if (siteSelectorButton) {
+    var siteSelector = document.getElementById('site-selector');
+    siteSelectorButton.addEventListener('click', function(event) {
+      siteSelector.classList.toggle('active');
+    }, true);
+    // siteSelectorButton.addEventListener('blur', function(event) {
+    //   siteSelector.classList.remove('active');
+    // }, true);
   }
+  // var menuItem = document.querySelectorAll('.dropdown-button');
+  // if (menuItem) {
+  //   for (var i = 0; i < menuItem.length; i++) {
+  //     var button = menuItem[i];
+  //     button.addEventListener('click', function(event) {
+  //       event.target.classList.toggle('dropdown-active');
+  //     });
+  //     button.addEventListener('blur', function(event) {
+  //       event.target.classList.remove('dropdown-active');
+  //     });
+  //   }
+  // }
 });
