62200ed64bbbe2b5d651142a6e59135969a13990
kurt <kurt@rockcreektechnologiesllc.com>
2023-05-16 02:19:10 -0600

    link selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
-// alert('woah');
-// console.log('hi');
 window.addEventListener('DOMContentLoaded', function(domEvent) {
   var siteSelectorButton = document.getElementById('site-selector-button');
+  var selectorFlag = document.getElementById('flag');
+  var selectorIdentifier = document.getElementById('identifier');
+  var siteOptions = document.querySelectorAll('.options');
   if (siteSelectorButton) {
     var siteSelector = document.getElementById('site-selector');
     siteSelectorButton.addEventListener('click', function(event) {
       siteSelector.classList.toggle('active');
-    }, true);
-    // siteSelectorButton.addEventListener('blur', function(event) {
-    //   siteSelector.classList.remove('active');
-    // }, true);
+    });
   }
-  // var menuItem = document.querySelectorAll('.dropdown-button');
-  // if (menuItem) {
-  //   for (var i = 0; i < menuItem.length; i++) {
-  //     var button = menuItem[i];
-  //     button.addEventListener('click', function(event) {
-  //       event.target.classList.toggle('dropdown-active');
-  //     });
-  //     button.addEventListener('blur', function(event) {
-  //       event.target.classList.remove('dropdown-active');
-  //     });
-  //   }
-  // }
+  if (siteOptions.length > 0) {
+    for (var i = 0; i < siteOptions.length; i++) {
+      siteOptions[i].addEventListener('click', function(event) {
+        var ident = event.currentTarget.dataset.identifier;
+        siteSelector.classList.remove('active');
+        if (identifier.innerHTML === ident) {
+          return;
+        }
+        var domain = event.currentTarget.dataset.domain;
+        var flag = event.currentTarget.children[0].firstElementChild.firstElementChild.innerText;
+        selectorIdentifier.innerHTML = ident;
+        selectorFlag.innerText = flag;
+        var swapLinks = document.querySelectorAll('.swap-link');
+        for (var j = 0; j < swapLinks.length; j++) {
+          var newUrl = new URL(swapLinks[j].href);
+          newUrl.hostname = domain;
+          swapLinks[j].href = newUrl.href;
+        }
+      }, { capture: true });
+    }
+  }
 });
